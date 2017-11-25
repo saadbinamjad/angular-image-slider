1fdfd9ee5fa0faa5bdfc464f97aa17ec86b5b2ba
saadbinamjad <saadbinamjad@gmail.com>
2017-11-25 13:00:36 +0600

    added slider functions

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -9,12 +9,19 @@ import { animate, state, style, transition, trigger, keyframes } from '@angular/
   animations: [
     trigger('move', [
       state('in', style({ transform: 'translateX(0)' })),
-      transition('void => *', [
+      transition('void => left', [
         style({ transform: 'translateX(100%)' }),
         animate(100)
       ]),
-      transition('* => void', [
+      transition('left => void', [
         animate(100, style({ transform: 'translateX(-100%)' }))
+      ]),
+      transition('void => right', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(100)
+      ]),
+      transition('right => void', [
+        animate(100, style({ transform: 'translateX(100%)' }))
       ])
     ])
   ]
@@ -24,24 +31,40 @@ export class SliderComponent implements OnInit {
   constructor() { }
 
   @Input() images: [any];
-  public data: any;
+
   public imageUrls: any;
-  state = 'void';
+  public state = 'void';
 
   ngOnInit() {
-    this.data = this.images;
-    this.imageUrls = this.data.reverse();
+    this.imageUrls = this.images;
+  }
+
+  imageRotate(arr, reverse) {
+    if (reverse)
+      arr.unshift(arr.pop())
+    else
+      arr.push(arr.shift())
+    return arr;
   }
 
   moveLeft() {
-    this.state = 'in';
-    this.imageUrls.shift();
+    this.state = 'left';
+    this.imageRotate(this.imageUrls, false)
+  }
+
+  moveRight() {
+    this.state = 'right';
+    this.imageRotate(this.imageUrls, true)
   }
 
   onFinish($event) {
     this.state = 'void';
   }
 
-  onStart($event) {
-  }
+  // If any start hooks are required
+  // template: (@move.start)="onStart($event)" 
+  // onStart($event) {
+  //   //No call
+  // }
+
 }
