2d3807f89f83799f73e2c6cde6670c51fc859bfe
saadbinamjad <saadbinamjad@gmail.com>
2017-11-25 18:25:58 +0600

    added overlay for left and right image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,10 @@ export class AppComponent {
 
   ngOnInit() {
     this.imagesUrl = [
-      'https://upload.wikimedia.org/wikipedia/en/e/eb/SupermanRoss.png',
-      'https://pre00.deviantart.net/ad63/th/pre/i/2015/023/2/4/batman_by_dawidarte-d801eel.png',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6SLdo3D9PNxVSqM5MqW9jKVHe3uKLNPthnLQ4_d-zYH4OQbmG',
-      'https://i.pinimg.com/736x/2f/f2/7f/2ff27fdd4eda3e8d99053e7093a76aba--dc-comic-comic-art.jpg',
-      'https://i.pinimg.com/736x/ef/40/67/ef4067ed4dd41332360659025913017e--cyborg-dc-steam-room.jpg',
+      'http://www.telegraph.co.uk/content/dam/motoring2/2015/12/07/01-Kia-Sportage-front-xlarge_trans_NvBQzQNjv4BqrWYeUU_H0zBKyvljOo6zlkYMapKPjdhyLnv9ax6_too.jpg',
+      'http://www.telegraph.co.uk/cars/images/2017/01/24/A5-Sportback-main-xlarge_trans_NvBQzQNjv4BqZR6q1BRVjLLZ5nciTmZ6ABYYy2HF4Csw_oYIEcbI_AA.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPopqXeuO7fqot51N7vaZuh9EqBYgZkLexcmQ_A0Fy0CjjW6J',
+      'https://www.cars.co.za/carimages_gen/Audi-TT/Audi-TT-coupe-1.8TFSI_AudiTT3c6l.jpg',
       'http://comicsalliance.com/files/2011/04/strips02.jpg',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq4HTtZrfKqNo5riVYiOBBL7-9laaPZcW1RfDfYGvb6BezfMtQ',
       'https://s-media-cache-ak0.pinimg.com/originals/73/f3/08/73f30861d214eea1d6c5d99fe72b3053.jpg',
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -11,17 +11,17 @@ import { animate, state, style, transition, trigger, keyframes } from '@angular/
       state('in', style({ transform: 'translateX(0)' })),
       transition('void => left', [
         style({ transform: 'translateX(100%)' }),
-        animate(100)
+        animate(200)
       ]),
       transition('left => void', [
-        animate(100, style({ transform: 'translateX(-100%)' }))
+        animate(200, style({ transform: 'translateX(0)' }))
       ]),
       transition('void => right', [
         style({ transform: 'translateX(-100%)' }),
-        animate(100)
+        animate(200)
       ]),
       transition('right => void', [
-        animate(100, style({ transform: 'translateX(100%)' }))
+        animate(200, style({ transform: 'translateX(0)' }))
       ])
     ])
   ]
